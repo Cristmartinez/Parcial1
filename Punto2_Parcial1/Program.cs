3a49cbeae88eeb243045d686eb3c0bd5ced33078
Cristian Martinez <cristianmartinez97@hotmail.com>
2024-03-08 21:46:58 -0500

    Actualizacion de variables

---
+++ Punto2_Parcial1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Punto2_Parcial1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Velocidad de los trenes 
+            double VelocidadTren1 = 80;
+            double VelocidadTren2 = 100;
+
+            //Inicio Recorrido 
+            double Tiempoincial = 10;
+
+            double DistanciaEntreTrenes = VelocidadTren1 * Tiempoincial;
+
+            double timeToCatchUp = DistanciaEntreTrenes / (VelocidadTren2 - VelocidadTren1);
+
+            double catchUpTime = timeToCatchUp + 10;
+
+            Console.WriteLine($"El segundo tren alcanzará al primer tren a las {catchUpTime}");
+        }
+    }
+}
+
