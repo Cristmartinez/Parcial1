880376ada49042c0d08a8c58d73fca48f843d7c4
Cristian Martinez <cristianmartinez97@hotmail.com>
2024-03-08 20:36:07 -0500

    Prueba Commit

---
+++ Punto1_Parcial1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Punto1_Parcial1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
